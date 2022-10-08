37f1ceddc0877e8c5004126fd300ee6786d012e4
xubinzha <xubinzha@usc.edu>
2022-10-08 13:55:23 -0700

    Camera

--- ChestNut/Assets/Scripts/Follower.cs
+++ ChestNut/Assets/Scripts/Follower.cs
@@ -5,62 +5,84 @@ using UnityEngine;
 public class Follower : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset = new Vector3(0, 3, -5);
     public float zoom_in_param = 1.25f;
     public float zoom_out_param = 1.25f;
-
-    public float zoom_out_max = 20f;
     public bool is_active = false;  // is in use
+    public float rotateDeltaX = 1f;
+    public float rotateDeltaY = 3f;
+    public float distance = 5f;
+    public float minDistance;
+    public float maxDistance = 20f;
+    public float myRotationX = 30f;
+    public float myRotationY = 0f;
+    public float minAngleX = 10f;
+    public float maxAngleX = 90f;
+    public float damping = 10f;
+    public int mode = 0;    // 0: normal; 1: topdown
     
-    private Vector3 orignal_offset;
+    private float orignal_distance;
     private Player p;
-    private float myTime = 0.0f;
-    private float nextRotate = 0.4f;
+    // private float myTime = 0.0f;
+    // private float nextRotate = 0.4f;
 
     // Start is called before the first frame update
     void Start()
     {
         p = FindObjectOfType<Player>();
-        orignal_offset = offset;
+        orignal_distance = distance;
+        minDistance = p.getRadius();
+        if (mode == 1) {
+            minAngleX = 90f;
+        }
+        cameraTransformation();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        myTime = myTime + Time.deltaTime;
-        while (offset.magnitude < p.getRadius()) {
-            offset = new Vector3(offset.x * zoom_out_param, offset.y * zoom_out_param, offset.z * zoom_out_param);
+        minDistance = p.getRadius();
+
+        // zooming
+        while (distance < minDistance) {
+            distance *= zoom_out_param;
         }
-        if (Input.GetKey("=") && offset.magnitude / zoom_in_param > p.getRadius()) {    // zoom in
-            offset = new Vector3(offset.x / zoom_in_param, offset.y / zoom_in_param, offset.z / zoom_in_param);
+        if (Input.GetKey("=") && distance / zoom_in_param > minDistance) {    // zoom in
+            distance /= zoom_in_param;
         }
-        else if (Input.GetKey("-") && offset.magnitude * zoom_out_param < zoom_out_max) {    // zoom out
-            offset = new Vector3(offset.x * zoom_out_param, offset.y * zoom_out_param, offset.z * zoom_out_param);
+        else if (Input.GetKey("-") && distance * zoom_out_param < maxDistance) {    // zoom out
+            distance *= zoom_out_param;
         }
-        else if (Input.GetKey("0") && orignal_offset.magnitude > p.getRadius()) {
-            offset = orignal_offset;
+        else if (Input.GetKey("0") && orignal_distance > minDistance) {
+            distance = orignal_distance;
         }
-        else if (Input.GetKey("l") && myTime > nextRotate) {    // rotate right
-            myTime = 0.0f;
-            offset = Quaternion.Euler(0, 90, 0) * offset;
-            var rot = transform.rotation.eulerAngles;
-            rot.y += 90;
-            transform.rotation = Quaternion.Euler(rot);
+
+        // rotation
+        if (Input.GetKey("l")) {    // rotate right
+            myRotationY += rotateDeltaY;
             if (is_active == true) {
-                p.force_direction_shift(90);
+                p.force_direction_shift(rotateDeltaY);
             }
         }
-        else if (Input.GetKey("j") && myTime > nextRotate) {    // rotate left
-            myTime = 0.0f;
-            offset = Quaternion.Euler(0, -90, 0) * offset;
-            var rot = transform.rotation.eulerAngles;
-            rot.y -= 90;
-            transform.rotation = Quaternion.Euler(rot);
+        if (Input.GetKey("j")) {    // rotate left
+            myRotationY -= rotateDeltaY;
             if (is_active == true) {
-                p.force_direction_shift(-90);
+                p.force_direction_shift(-rotateDeltaY);
             }
         }
-        // Offset camera
-        transform.position = player.transform.position + offset;
+        if (Input.GetKey("i")) {    // rotate up
+            myRotationX += rotateDeltaX;
+        }
+        if (Input.GetKey("k")) {    // rotate down
+            myRotationX -= rotateDeltaX;
+        }
+        myRotationX = Mathf.Clamp(myRotationX, minAngleX, maxAngleX);
+        cameraTransformation();
+    }
+
+    private void cameraTransformation() {
+        Quaternion myRotation = Quaternion.Euler(myRotationX, myRotationY, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, myRotation, Time.deltaTime * damping);
+        Vector3 myPosition = myRotation * new Vector3(0.0F, 0.0F, -distance) + player.transform.position;
+        transform.position = Vector3.Lerp(transform.position, myPosition, Time.deltaTime * damping);
     }
 }
