a895978da2edf7544529f31237910bd9c965df67
xubinzha <xubinzha@usc.edu>
2022-10-08 22:21:17 -0700

    Camera

--- ChestNut/Assets/Scripts/Follower.cs
+++ ChestNut/Assets/Scripts/Follower.cs
@@ -18,10 +18,13 @@ public class Follower : MonoBehaviour
     public float minAngleX = 10f;
     public float maxAngleX = 90f;
     public float damping = 10f;
-    public int mode = 0;    // 0: normal; 1: topdown
-    
+    public int mode = 0;    // 0: normal; 1: topdown; 2: first person
     private float orignal_distance;
     private Player p;
+    private bool zoom_enabled = true;
+    private bool xrotation_enabled = true;
+    private bool yrotation_enabled = true;
+    private bool damping_enabled = true;
     // private float myTime = 0.0f;
     // private float nextRotate = 0.4f;
 
@@ -31,8 +34,16 @@ public class Follower : MonoBehaviour
         p = FindObjectOfType<Player>();
         orignal_distance = distance;
         minDistance = p.getRadius();
-        if (mode == 1) {
-            minAngleX = 90f;
+        if (mode == 1) {    // topdown
+            myRotationX = 90f;
+            xrotation_enabled = false;
+        }
+        if (mode == 2) {    // first person
+            minAngleX = -10f;
+            maxAngleX = 20f;
+            zoom_enabled = false;
+            distance = minDistance;
+            damping_enabled = false;
         }
         cameraTransformation();
     }
@@ -41,48 +52,58 @@ public class Follower : MonoBehaviour
     void FixedUpdate()
     {
         minDistance = p.getRadius();
+        if (mode == 2) {
+            distance = p.getRadius();
+        }
 
         // zooming
-        while (distance < minDistance) {
-            distance *= zoom_out_param;
-        }
-        if (Input.GetKey("=") && distance / zoom_in_param > minDistance) {    // zoom in
-            distance /= zoom_in_param;
-        }
-        else if (Input.GetKey("-") && distance * zoom_out_param < maxDistance) {    // zoom out
-            distance *= zoom_out_param;
-        }
-        else if (Input.GetKey("0") && orignal_distance > minDistance) {
-            distance = orignal_distance;
+        if (zoom_enabled){
+            if (Input.GetKey("=")) {    // zoom in
+                distance /= zoom_in_param;
+            }
+            else if (Input.GetKey("-")) {    // zoom out
+                distance *= zoom_out_param;
+            }
+            else if (Input.GetKey("0")) {
+                distance = orignal_distance;
+            }
         }
 
         // rotation
-        if (Input.GetKey("l")) {    // rotate right
-            myRotationY += rotateDeltaY;
-            if (is_active == true) {
-                p.force_direction_shift(rotateDeltaY);
+        if (yrotation_enabled) {
+            if (Input.GetKey("l")) {    // rotate right
+                myRotationY += rotateDeltaY;
+                if (is_active == true) {
+                    p.force_direction_shift(rotateDeltaY);
+                }
+            }
+            if (Input.GetKey("j")) {    // rotate left
+                myRotationY -= rotateDeltaY;
+                if (is_active == true) {
+                    p.force_direction_shift(-rotateDeltaY);
+                }
             }
         }
-        if (Input.GetKey("j")) {    // rotate left
-            myRotationY -= rotateDeltaY;
-            if (is_active == true) {
-                p.force_direction_shift(-rotateDeltaY);
+        if (xrotation_enabled) {
+            if (Input.GetKey("i")) {    // rotate up
+                myRotationX += rotateDeltaX;
+            }
+            if (Input.GetKey("k")) {    // rotate down
+                myRotationX -= rotateDeltaX;
             }
         }
-        if (Input.GetKey("i")) {    // rotate up
-            myRotationX += rotateDeltaX;
-        }
-        if (Input.GetKey("k")) {    // rotate down
-            myRotationX -= rotateDeltaX;
-        }
-        myRotationX = Mathf.Clamp(myRotationX, minAngleX, maxAngleX);
         cameraTransformation();
     }
 
     private void cameraTransformation() {
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+        myRotationX = Mathf.Clamp(myRotationX, minAngleX, maxAngleX);
         Quaternion myRotation = Quaternion.Euler(myRotationX, myRotationY, 0);
-        transform.rotation = Quaternion.Lerp(transform.rotation, myRotation, Time.deltaTime * damping);
-        Vector3 myPosition = myRotation * new Vector3(0.0F, 0.0F, -distance) + player.transform.position;
-        transform.position = Vector3.Lerp(transform.position, myPosition, Time.deltaTime * damping);
+        transform.rotation = damping_enabled ? Quaternion.Lerp(transform.rotation, myRotation, Time.deltaTime * damping) : myRotation;
+        Vector3 myPosition = myRotation * new Vector3(0, 0, -distance) + player.transform.position;
+        if (mode == 2) {
+            myPosition += new Vector3(0, distance / 2, 0);
+        }
+        transform.position = damping_enabled ? Vector3.Lerp(transform.position, myPosition, Time.deltaTime * damping) : myPosition;
     }
 }
