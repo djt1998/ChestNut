a128b4f04b5bfb6443ecd2f6853809b2f88fad21
djt1998 <djt18919805555@gmail.com>
2022-10-28 14:58:23 -0700

    Add chest

--- ChestNut/Assets/AurynSky/WinterArena/Scripts/ChestDemo.cs
+++ ChestNut/Assets/AurynSky/WinterArena/Scripts/ChestDemo.cs
@@ -6,18 +6,31 @@ public class ChestDemo : MonoBehaviour
 {
 
     //This script goes on the ChestComplete prefab;
-
+    private Player player;
+    public float ratio;
+    public Rigidbody rb;
     public Animator chestAnim; //Animator for the chest;
+    public ParticleSystem death;
 
     // Use this for initialization
     void Awake()
     {
         //get the Animator component from the chest;
         chestAnim = GetComponent<Animator>();
+        player = FindObjectOfType<Player>();
+        rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+        rb.isKinematic = true;
         //start opening and closing the chest for demo purposes;
-        StartCoroutine(OpenCloseChest());
+        //StartCoroutine(OpenCloseChest());
     }
 
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.name == "Player_model")
+        {
+            StartCoroutine(OpenCloseChest());
+        }
+    }
 
     IEnumerator OpenCloseChest()
     {
@@ -25,12 +38,7 @@ public class ChestDemo : MonoBehaviour
         chestAnim.SetTrigger("open");
         //wait 2 seconds;
         yield return new WaitForSeconds(2);
-        //play close animation;
-        chestAnim.SetTrigger("close");
-        //wait 2 seconds;
-        yield return new WaitForSeconds(2);
-        //Do it again;
-        StartCoroutine(OpenCloseChest());
-
+        death.Play();
     }
+
 }
