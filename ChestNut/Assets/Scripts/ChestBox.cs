1e606744c44447f5a3554f7d66fabc0fcee716ad
xubinzha <xubinzha@usc.edu>
2022-10-28 17:31:18 -0700

    open chest anim

---
+++ ChestNut/Assets/Scripts/ChestBox.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestBox : MonoBehaviour
+{
+    public float ratio;
+    public Rigidbody rb;
+    public Animator chestAnim; //Animator for the chest;
+    private bool triggered;
+
+    // Use this for initialization
+    void Awake()
+    {
+        chestAnim = GetComponent<Animator>();
+        rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+        rb.isKinematic = true;
+        triggered = false;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (!triggered && other.name == "Player_model")
+        {
+            triggered= true;
+            StartCoroutine(OpenCloseChest());
+        }
+    }
+
+    IEnumerator OpenCloseChest()
+    {
+        //play open animation;
+        chestAnim.SetTrigger("open");
+        //wait 2 seconds;
+        yield return new WaitForSeconds(2);
+    }
+
+}
