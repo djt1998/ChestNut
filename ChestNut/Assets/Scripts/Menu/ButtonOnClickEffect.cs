7dcdf12e2154537ba4deac1a4cb252a2f29d2017
xubinzha <xubinzha@usc.edu>
2022-11-11 21:29:02 -0800

    Add button click sound effect

---
+++ ChestNut/Assets/Scripts/Menu/ButtonOnClickEffect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOnClickEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(ButtonClicked);
+    }
+
+    void ButtonClicked()
+    {
+        SoundEffectManger SEM = FindObjectOfType<SoundEffectManger>();
+        if (SEM) {
+            SEM.Play("ButtonClick");
+        }
+    }
+}
